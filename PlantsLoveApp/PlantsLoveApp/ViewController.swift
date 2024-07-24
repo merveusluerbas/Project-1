b18ad0980f39e04c3d1ed2ccc409bcdefbe93a53
Merve Uslu Erbas <merveusluerbas@Merves-MacBook-Pro.local>
2024-07-24 22:08:33 +0200

    Create plants love app project

---
+++ PlantsLoveApp/PlantsLoveApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantsLoveApp
+//
+//  Created by Merve Uslu Erbas on 24/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
